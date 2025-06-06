3ed1b056ff610ed70cb26993599a1955d5c2a35c
LeeMinGeon <alsrjs951@naver.com>
2025-06-07 02:04:13 +0900

    임시 커밋

--- apps/users/signals.py
+++ apps/users/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.contrib.auth import get_user_model
-from apps.recommender.models import UserPreferenceProfile
+from apps.users.models import UserPreferenceProfile
 
 User = get_user_model()
 
